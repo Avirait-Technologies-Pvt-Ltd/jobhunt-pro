847eca73271bd49c6e7330d210bd1f0bb89df927
avinashhubale <avinashhubale1@gmail.com>
2026-01-19 23:38:35 +0530

    Add skill assessment index file

---
+++ src/components/skill-assessment/index.ts
@@ -0,0 +1,6 @@
+export { AssessmentCard } from './AssessmentCard';
+export { AssessmentQuestion } from './AssessmentQuestion';
+export { AssessmentTimer } from './AssessmentTimer';
+export { AssessmentProgress } from './AssessmentProgress';
+export { AssessmentResult } from './AssessmentResult';
+export { SkillGapChart, JobMatchCard } from './SkillGapChart';
