98bc151508bfba969113041565d0ce2c28b9e30b
avinashhubale <avinashhubale1@gmail.com>
2026-01-23 07:30:22 +0530

    Add career goals index file

---
+++ src/components/career-goals/index.ts
@@ -0,0 +1,8 @@
+// Career Goals Components Barrel Export
+
+export { GoalCard } from './GoalCard';
+export { MilestoneTimeline } from './MilestoneTimeline';
+export { GoalForm } from './GoalForm';
+export { AchievementBadge, AchievementBadgeGrid } from './AchievementBadge';
+export { GoalAnalytics } from './GoalAnalytics';
+export { GoalFilters } from './GoalFilters';
